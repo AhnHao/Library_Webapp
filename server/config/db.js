6003884c994bd11de06d2efe17b44825170a78fa
AhnHao <haotrananh2@gmail.com>
2025-04-06 09:51:34 +0700

    add log api

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://mongo:27017/library_db', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('Kết nối MongoDB thành công');
-    } catch (err) {
-        console.error('Lỗi kết nối MongoDB:', err);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(
+      process.env.MONGODB_URI || "mongodb://mongodb:27017/library",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("Kết nối MongoDB thành công");
+  } catch (err) {
+    console.error("Lỗi kết nối MongoDB:", err);
+    process.exit(1);
+  }
 };
 
-module.exports = connectDB; 
+module.exports = connectDB;
